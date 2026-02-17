18e37a28d7e685e8b15ed9ab848d94df88d79343
kayla <kaylamcdon@hotmail.com>
2026-02-17 10:17:36 -0500

    feat: added the major arcana tarot cards

---
+++ badCode.py
@@ -0,0 +1,32 @@
+cards = {
+    "The Fool" : "New beginnings",
+    "The Magician" : "Resourcefulness",
+    "The High Priestess" : "Intuition",
+    "The Empress" : "Abundance",
+    "The Emperor" : "Leadership",
+    "The Hierophant" : "Tradition",
+    "The Lovers" : "Love",
+    "The Chariot" : "Success",
+    "Strength" : "Courage",
+    "The Hermit" : "Wisdom",
+    "TheWheel of Fortune" : "Luck",
+    "Justice" : "Truth",
+    "The Hanged Man" : "Letting go",
+    "Death" : "Transformation",
+    "Temperance" : "Balance",
+    "Devil" : "Temptation",
+    "The Tower" : "Sudden change",
+    "The Star" : "Hope",
+    "The Moon" : "Mystery",
+    "The Sun" : "Joy",
+    "Judgement" : "Self-reflection",
+    "The World" : "Success"
+}
+
+#introduction
+#ask question 1
+#draw a card
+#ask question 2
+#draw a card
+#ask question 3
+#draw a card
