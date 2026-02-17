66607e3a1e135705b6f3e14acbe8d2ae96bf3e30
kayla <kaylamcdon@hotmail.com>
2026-02-17 11:12:47 -0500

    feat: added asynchronous card shuffeling to ensure that when the player tells the fortune teller to stop they are recieving a card that they had control over

--- badCode.py
+++ badCode.py
@@ -1,4 +1,7 @@
-cards = {
+import random
+import asyncio
+
+myDict = {
     "The Fool" : "New beginnings",
     "The Magician" : "Resourcefulness",
     "The High Priestess" : "Intuition",
@@ -7,26 +10,58 @@ cards = {
     "The Hierophant" : "Tradition",
     "The Lovers" : "Love",
     "The Chariot" : "Success",
-    "Strength" : "Courage",
+    "The Strength" : "Courage",
     "The Hermit" : "Wisdom",
-    "TheWheel of Fortune" : "Luck",
-    "Justice" : "Truth",
+    "The Wheel of Fortune" : "Luck",
+    "The Justice" : "Truth",
     "The Hanged Man" : "Letting go",
-    "Death" : "Transformation",
-    "Temperance" : "Balance",
-    "Devil" : "Temptation",
+    "The Death" : "Transformation",
+    "The Temperance" : "Balance",
+    "The Devil" : "Temptation",
     "The Tower" : "Sudden change",
     "The Star" : "Hope",
     "The Moon" : "Mystery",
     "The Sun" : "Joy",
-    "Judgement" : "Self-reflection",
+    "The Judgement" : "Self-reflection",
     "The World" : "Success"
 }
 
-#introduction
-#ask question 1
-#draw a card
-#ask question 2
-#draw a card
-#ask question 3
-#draw a card
+global num
+num=0
+
+async def shuffle():
+    while(True):
+        global num
+        num = int((num+random.randint(0,4)) % len(myDict))
+        await asyncio.sleep(0.1)
+    
+
+
+async def story():
+    #introduction
+    print("You enter a tent and see an old women bent over a small table.")
+    await asyncio.sleep(0.5)
+    print('"Hello, and welcome to my humble tarot reading."')
+    await asyncio.sleep(0.5)
+    print("She starts to shuffle the deck of cards in front of her.")
+    await asyncio.sleep(0.5)
+    print('"Say the word whenever you want me to stop dearie"')
+    await asyncio.sleep(0.5)
+    x = input(" > ")
+    await asyncio.sleep(0.5)
+    print("hmmm, interesting")
+    await asyncio.sleep(0.5)
+    print("AGAAGA: " + str(num))
+    print(f"you drew {list(myDict)[num]} card. This card represents {myDict[list(myDict)[num]]}")
+
+    #ask question 1
+    #draw a card
+    #ask question 2
+    #draw a card
+    #ask question 3
+    #draw a card
+
+async def main():
+    await asyncio.gather(shuffle(), story())
+
+asyncio.run(main())
